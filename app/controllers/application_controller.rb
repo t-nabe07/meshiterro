d253dc0781a6770a4e7df30fd03978dcad55897e
EC2 Default User <ec2-user@ip-172-31-40-17.ec2.internal>
2021-11-13 00:15:49 +0000

    [Update]4-5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!,except: [:top]
+  #↑ログインしていないとログイン画面にリダイレクト
+
+  #deviseのコントローラーはライブラリで用意されているので直接修正ができない。
+  #deviseのコントローラーに修正が必要ならばここに記述する。
+  #カラム(name)をuserテーブルに追加しているので追記する。
+
+  before_action :configure_permitted_parameters,if: :devise_controller?
+  #devise利用の機能（ユーザー登録、ログイン認証など）が使われる場合、
+  #その前にconfigure_permitted_parametersが実行されます
+
+  def after_sign_in_path_for(resource)
+    post_images_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:name])
+  end
+
+  #configure_permitted_parametersでは、devise_parameter_sanitizer.permitで
+  #nameのデータ操作を許可するアクションメソッドが指定されています。
+  #今回のケースでは、ユーザ登録（sign_up）の際に、ユーザ名（name）の
+  #データ操作が許可されます。
+
+  #これは、CARAVAN作成時のStrong Parametersと同様の機能です。
+  #privateは、自分のコントローラ内でしか参照できません。
+  #一方、protectedは呼び出された他のコントローラからも参照できます。
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1bf758d4170715e3ce8d77e82bc45fddbe80f96e7dee85100ea328253277b488e0413917da78d1f6434834489e37498cea34132e0cb44dc75d43d1d9c79b487d'
